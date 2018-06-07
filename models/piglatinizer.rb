f812eba8576418eec61e792c81708e5802145eb5
Eddie Moon <eddiemoonie@gmail.com>
2018-06-07 21:07:11 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -16,7 +16,7 @@ class PigLatinizer
 
   def piglatinize_word(word)
     if !consonant?(word[0])
-      word = word + "w"
+      word = word << "w"
     elsif consonant?(word[0]) && consonant?(word[1]) && consonant?(word[2])
       word = word.slice(3..-1) + word.slice(0,3)
     elsif consonant?(word[0]) && consonant?(word[1])
