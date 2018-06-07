f099087965569041b5a56766dfadc4cdbf412d78
Eddie Moon <eddiemoonie@gmail.com>
2018-06-07 21:13:09 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -21,7 +21,7 @@ class PigLatinizer
       word = word.slice(3..-1) + word.slice(0,3)
     elsif consonant?(word[0]) && consonant?(word[1])
       word = word.slice(2..-1) + word.slice(0,2)
-    else consonant?(word[0])
+    else
       word = word.slice(1..-1) + word.slice(0)
     end
 
@@ -31,6 +31,5 @@ class PigLatinizer
   def piglatinize_sentence(sentence)
     sentence.split.collect {|word| piglatinize_word(word)}.join(" ")
   end
-
 
 end
